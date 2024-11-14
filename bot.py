e19144e0f001a113f1fd24a42051aa027d281324
Sina <cnah.num1@gmail.com>
2024-11-14 18:11:06 +0330

    added tutorial videos

--- bot.py
+++ bot.py
@@ -3,15 +3,18 @@ from telebot import types
 from telebot.types import ForceReply
 from time import sleep
 from re import findall
+from dotenv import load_dotenv
+import os
+import json
 #====================================== 
-TOKEN = "000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" # Telegram token
-bot = telebot.TeleBot(TOKEN, parse_mode=None) 
-#======================================
-f = open("texts/admins_id.txt", "r")
-admins_id_text = f.read()
-admins_id_list = findall(r'\d+', admins_id_text)
-f.close()
+load_dotenv()
 
+api_token = os.getenv("API_TOKEN")  
+TOKEN = api_token # Telegram token
+bot = telebot.TeleBot(TOKEN, parse_mode=None) 
+
+admins_id_list = os.getenv("ADMIN_IDS").split(",")
+#======================================
 f = open("texts/suggestion_01.txt", "rb")
 suggestion_01 = f.read()
 f.close()
@@ -47,33 +50,57 @@ f.close()
 f = open("texts/kanal_haye_mohem.txt", "rb")
 kanal_haye_mohem_text = f.read()
 f.close()
+
+f = open("texts/about_bot.txt", "rb")
+about_bot_text = f.read()
+f.close()
 #======================================
-tizer_url = "https://link-to-file/ieee-bot/tizer.mp4"
-chart_url = "https://link-to-file/ieee-bot/chart.pdf"
-taghvim_url = "https://link-to-file/ieee-bot/taghvim.pdf"
-uni_map_url = "https://link-to-file/ieee-bot/uni-map.jpg"
-engineering_map_url = "https://link-to-file/ieee-bot/engineering-map.jpg"
+# Load the JSON file
+with open("directory_links.json", "r") as file:
+    data = json.load(file)
+# Root directory links
+calendar_url = data["root"]["calendar.jpg"]
+course_chart_url = data["root"]["course_chart.pdf"]
+faculty_map_url = data["root"]["faculty_map.pdf"]
+introduction_video_url = data["root"]["introduction_video.mp4"]
+university_map_url = data["root"]["university_map.jpg"]
+# Portal directory links
+# Educational
+class_schedule_url = data["portal"]["educational"]["class_schedule.mp4"]
+educational_requests_url = data["portal"]["educational"]["educational_requests.mp4"]
+exam_schedule_url = data["portal"]["educational"]["exam_schedule.mp4"]
+teacher_evaluation_url = data["portal"]["educational"]["teacher_evaluation.mp4"]
+# Financial Support
+online_payment_url = data["portal"]["financial_support"]["online_payment.mp4"]
+portal_password_url = data["portal"]["financial_support"]["portal_password.mp4"]
+# Research
+book_loan_url = data["portal"]["research"]["book_loan.mp4"]
+# Student Affairs
+meal_reservation_url = data["portal"]["student_affairs"]["meal_reservation.mp4"]
+sports_services_url = data["portal"]["student_affairs"]["sports_services.mp4"]
 #======================================
-markup_00 = types.ReplyKeyboardMarkup()
+markup_00 = types.ReplyKeyboardMarkup(resize_keyboard=True)
 itembtna_00 = types.KeyboardButton('IEEE')
 itembtnb_00 = types.KeyboardButton('رویدادها')
 itembtnc_00 = types.KeyboardButton('فرصت‌های همکاری')
 itembtnd_00 = types.KeyboardButton('دانشجو راهنما')
 itembtne_00 = types.KeyboardButton('ارتباط با اعضا')
 itembtnf_00 = types.KeyboardButton('انتقادات و پیشنهادات')
+itembtng_00 = types.KeyboardButton('درباره بات')
 markup_00.row(itembtna_00)
 markup_00.row(itembtnb_00,itembtnc_00)
 markup_00.row(itembtnd_00,itembtne_00)
 markup_00.row(itembtnf_00)
+markup_00.row(itembtng_00)
 
-markup_02 = types.ReplyKeyboardMarkup()
+markup_02 = types.ReplyKeyboardMarkup(resize_keyboard=True)
 itembtna_02 = types.KeyboardButton('رویداد های برگزار شده')
 itembtnb_02 = types.KeyboardButton('رویداد های جاری')
 itembtnc_02 = types.KeyboardButton('بازگشت')
 markup_02.row(itembtnb_02, itembtna_02)
 markup_02.row(itembtnc_02)
 
-markup_04 = types.ReplyKeyboardMarkup()
+markup_04 = types.ReplyKeyboardMarkup(resize_keyboard=True)
 itembtna_04 = types.KeyboardButton("آموزش پرتال")
 itembtnb_04 = types.KeyboardButton("چارت درسی")
 itembtnc_04 = types.KeyboardButton("آخرین تقویم آموزشی")
@@ -87,7 +114,7 @@ markup_04.row(itembtnd_04,itembtne_04)
 markup_04.row(itembtnf_04)
 markup_04.row(itembtng_04)
 
-markup_05 = types.ReplyKeyboardMarkup()
+markup_05 = types.ReplyKeyboardMarkup(resize_keyboard=True)
 itembtna_05 = types.KeyboardButton("آموزشی")
 itembtnb_05 = types.KeyboardButton("پژوهشی")
 itembtnc_05 = types.KeyboardButton("دانشجویی")
@@ -98,6 +125,10 @@ markup_05.row(itembtna_05, itembtnb_05)
 markup_05.row(itembtnc_05, itembtnd_05)
 markup_05.row(itembtne_05)
 markup_05.row(itembtnf_05)
+
+markup_06 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+itembtna_06 = types.KeyboardButton("Cancel")
+markup_06.row(itembtna_06)
 #======================================
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -109,7 +140,7 @@ def echo_all(message):
 		bot.reply_to(message, "🏠", reply_markup=markup_00)
 
 	if message.text == "IEEE":
-		bot.send_video(chat_id=message.chat.id, video=tizer_url, caption=ieee_text)
+		bot.send_video(chat_id=message.chat.id, video=introduction_video_url, caption=ieee_text)
 		
 	if message.text == "رویدادها":
 		bot.reply_to(message, event_text, reply_markup=markup_02)
@@ -133,41 +164,52 @@ def echo_all(message):
 		bot.reply_to(message, "choose one the options", reply_markup=markup_05)
 
 	if message.text == "چارت درسی":
-		bot.send_document(chat_id=message.chat.id, document=chart_url)
+		bot.send_document(chat_id=message.chat.id, document=course_chart_url)
 
 	if message.text == "آخرین تقویم آموزشی":
-		bot.send_document(chat_id=message.chat.id, document=taghvim_url)
+		bot.send_photo(chat_id=message.chat.id, photo=calendar_url)
 
 	if message.text == "نقشه دانشگاه":
-		bot.send_photo(chat_id=message.chat.id, photo=uni_map_url)
+		bot.send_photo(chat_id=message.chat.id, photo=university_map_url)
 
 	if message.text == "نقشه دانشکده":
-		bot.send_photo(chat_id=message.chat.id, photo=engineering_map_url)
+		bot.send_document(chat_id=message.chat.id, document=faculty_map_url)
 
 	if message.text == "کانال های مهم دانشگاه":
 		bot.reply_to(message, kanal_haye_mohem_text)
 
 	if message.text == "آموزشی":
-		bot.reply_to(message, "under construction")
+		bot.send_video(chat_id=message.chat.id, video=class_schedule_url, caption="Educational 1")
+		#bot.send_video(chat_id=message.chat.id, video=educational_requests_url, caption="Educational 2")
+		bot.send_video(chat_id=message.chat.id, video=exam_schedule_url, caption="Educational 3")
+		bot.send_video(chat_id=message.chat.id, video=teacher_evaluation_url, caption="Educational 4")
 
 	if message.text == "پژوهشی":
-		bot.reply_to(message, "under construction")
+		bot.send_video(chat_id=message.chat.id, video=book_loan_url, caption="Research")
 
 	if message.text == "دانشجویی":
-		bot.reply_to(message, "under construction")
+		bot.send_video(chat_id=message.chat.id, video=meal_reservation_url, caption="Student Affairs 1")
+		bot.send_video(chat_id=message.chat.id, video=sports_services_url, caption="Student Affairs 2")
 
 	if message.text == "مالی و پشتیبانی":
-		bot.reply_to(message, "under construction")
+		bot.send_video(chat_id=message.chat.id, video=online_payment_url, caption="Financial & Support 1")
+		bot.send_video(chat_id=message.chat.id, video=portal_password_url, caption="Financial & Support 2")
 
 	if message.text == "سایر موارد":
 		bot.reply_to(message, "under construction")
 		
 	if message.text == "انتقادات و پیشنهادات":
-		bot.reply_to(message, suggestion_01, reply_markup=ForceReply())
+		bot.reply_to(message, suggestion_01, reply_markup=markup_06)
 		bot.register_next_step_handler(message, suggestion)
+
+	if message.text == 'درباره بات':
+		bot.reply_to(message, about_bot_text)
 
 def suggestion(message):
 	suggestion_text = message.text
+	if suggestion_text == "Cancel":
+		bot.reply_to(message, "Canceled", reply_markup=markup_00)
+		return
 	try: 
 		for id in admins_id_list:
 			bot.send_message(id, "NEW SUGGESTION:\n\n"+suggestion_text)
